f9a27c6e36a02312980c2f72272107c49401e991
Jan Špecián <specian@mailwwm.com>
2022-01-11 14:38:44 +0100

    pridan zakladni auth guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,23 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CrisisCenterModule } from './crisis-center/crisis-center.module';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
+import { AdminModule } from './admin/admin.module';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    ComposeMessageComponent
+    ComposeMessageComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HeroesModule,
     CrisisCenterModule,
+    AdminModule,
+    AuthModule,
     AppRoutingModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
